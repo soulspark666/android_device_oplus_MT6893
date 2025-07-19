04f6c15eec31bd56c6109222dbe9afbda945ab56
Soul Spark <spark_soul@mail.ru>
2025-07-19 09:44:45 +0000

    feat: adapt to voltage and add voltage config with maintainer name

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_MT6893.mk
+    $(LOCAL_DIR)/voltage_MT6893.mk
--- voltage_MT6893.mk
+++ voltage_MT6893.mk
@@ -22,10 +22,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oplus/MT6893/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_MT6893
+PRODUCT_NAME := voltage_MT6893
 PRODUCT_DEVICE := MT6893
 PRODUCT_BRAND := Oplus
 PRODUCT_MODEL := MT6893
