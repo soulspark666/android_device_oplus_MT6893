e7c4ab90b4f862260b88f81fd23d60c4a281d2a7
mikeNG <mikeNG@users.noreply.github.com>
2025-07-20 11:27:20 +0000

    cupida: Move Lineage Health HAL to select() *  vendor/lineage/config/BoardConfigSoong.mk:50: warning:  TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH is deprecated, please migrate to  soong_config_set,lineage_health,charging_control_charging_path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -193,6 +193,5 @@ WIFI_DRIVER_STATE_OFF := 0
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 
-# Lineage Health
-TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/devices/virtual/oplus_chg/battery/mmi_charging_enable
-
+# Health
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/devices/virtual/oplus_chg/battery/mmi_charging_enable)
